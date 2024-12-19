14d70c1a4bb70067af9e3c0d87a13458c71ceb73
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-12-19 10:05:46 -0300

    build: add dotent

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UploadModule } from './upload/upload.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UploadModule],
+  imports: [UploadModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
